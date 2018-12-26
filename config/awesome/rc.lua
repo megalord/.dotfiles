d60dc15edefecd77b7a53b10d59b214d4dfc1681
megalord <jordan.griege@rackspace.com>
2018-12-26 14:42:03 -0600

    lovelace theme

--- config/awesome/rc.lua
+++ config/awesome/rc.lua
@@ -41,7 +41,7 @@ end
 
 -- {{{ Variable definitions
 -- Themes define colours, icons, font and wallpapers.
-beautiful.init(gears.filesystem.get_themes_dir() .. "default/theme.lua")
+beautiful.init(os.getenv("HOME") .. "/.config/awesome/themes/lovelace/theme.lua")
 
 -- This is used later as the default terminal and editor to run.
 terminal = "xterm"
--- link.sh
+++ link.sh
@@ -1,11 +1,20 @@
 #!/bin/bash
+
+CONFIG="~/.config"
+
 for f in $(find . -maxdepth 1 -type f -name '.*'); do
-  ln -s ~/.dotfiles/$f ~/$f
+  ln -s $PWD/$f ~/$f
 done
 
 mkdir -p ~/.config
 for f in $(cd config && find . -mindepth 1 -maxdepth 1); do
-  ln -s ~/.dotfiles/config/$f ~/.config/$f
+  ln -s $PWD/config/$f $CONFIG/$f
 done
+
+if [ ! -d elenapan-dotfiles ]; then
+  git clone https://github.com/elenapan/dotfiles elenapan-dotfiles
+  ln -s $PWD/elenapan-dotfiles/config/awesome/themes/lovelace $PWD/config/awesome/themes/lovelace
+  ln -s $PWD/elenapan-dotfiles/config/awesome/noodle $PWD/config/awesome/noodle
+fi
 
 sudo dnf install $(grep '^[^#]' packages)
